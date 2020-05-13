48b0039ab0e4a734f83803599d5448f747eee731
Sylvio Sell <info@maitag.de>
2020-05-13 05:51:57 +0200

    fixing tabs in hxp benchmarks script

--- benchmarks.hx
+++ benchmarks.hx
@@ -2,34 +2,34 @@ import hxp.*;
 
 class Benchmarks extends hxp.Script
 {
-    public function new()
-    {
-        super();
+	public function new()
+	{
+		super();
 
-        run("testing with Vector:");
-        run("testing with Array:", ["-D", "PixelMatrixUseArray"]);
-        run("testing with UInt32Array:", ["-D", "PixelMatrixUseUInt32Array"]);
-        run("testing with Bytes:", ["-D", "PixelMatrixUseBytes"]);
+		run("testing with Vector:");
+		run("testing with Array:", ["-D", "PixelMatrixUseArray"]);
+		run("testing with UInt32Array:", ["-D", "PixelMatrixUseUInt32Array"]);
+		run("testing with Bytes:", ["-D", "PixelMatrixUseBytes"]);
     }
 
-    function run(desc, args = null)
-    {
-        Log.info(desc);
+	function run(desc, args = null)
+	{
+		Log.info(desc);
 
-        var arguments = [ "benchmarks.hxml" ];
-        if (args != null) arguments = arguments.concat(args);
-        		
+		var arguments = [ "benchmarks.hxml" ];
+		if (args != null) arguments = arguments.concat(args);
+				
 		System.runCommand("", "haxe", arguments);
 
-        var outputDir = "Export/benchmarks";
+		var outputDir = "Export/benchmarks";
 
-        Log.info("------------------- Node -------------------------------------");
-        System.runCommand(outputDir, "node", ["benchmarks.js"]);
+		Log.info("------------------- Node -------------------------------------");
+		System.runCommand(outputDir, "node", ["benchmarks.js"]);
 
-        Log.info("------------------- CPP --------------------------------------");
-        System.runCommand(outputDir, ((System.hostPlatform != WINDOWS) ? "./" : "") + "Benchmarks", []);
-        
+		Log.info("------------------- CPP --------------------------------------");
+		System.runCommand(outputDir, ((System.hostPlatform != WINDOWS) ? "./" : "") + "Benchmarks", []);
+
 		Log.info("------------------- Neko -------------------------------------");
-        System.runCommand(outputDir, "neko", [ "benchmarks.n" ]);
-    }
+		System.runCommand(outputDir, "neko", [ "benchmarks.n" ]);
+	}
 }
